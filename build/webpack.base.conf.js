58e8d5e5a5828ad5a7f5c7258277e8facdc9d48a
aisteron <timotheus@list.ru>
2024-03-07 12:12:03 +0300

    clear, prepare to react

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { VueLoaderPlugin } = require('vue-loader')
+
 
 // Main const
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#main-const
@@ -72,15 +72,8 @@ module.exports = {
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: '/node_modules/'
-    }, {
-      test: /\.vue$/,
-      loader: 'vue-loader',
-      options: {
-        loader: {
-          scss: 'vue-style-loader!css-loader!sass-loader'
-        }
-      }
-    }, {
+    },
+		{
       test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
       loader: 'file-loader',
       options: {
@@ -93,7 +86,6 @@ module.exports = {
         name: '[name].[ext]'
       }
     }, {
-      //test: /\.scss$/,
       test: /\.(sass|scss)$/,
       use: [
         'style-loader',
@@ -127,11 +119,11 @@ module.exports = {
   resolve: {
     alias: {
       '~': PATHS.src,
-      'vue$': 'vue/dist/vue.js',
+
     }
   },
   plugins: [
-    new VueLoaderPlugin(),
+
     new MiniCssExtractPlugin({
       filename: `${PATHS.assets}css/[name].[hash].css`,
     }),
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "jest-puppeteer",
-  globalSetup: './cfg/setup.js',
-  globalTeardown: './cfg/teardown.js',
-  //testEnvironment: './cfg/puppeteer_environment.js',
-	testEnvironment: 'node'
-};
