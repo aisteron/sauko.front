b4a0c36f7dfe387ce5a97940a1436ac53111f457
aisteron <timotheus@list.ru>
2024-03-22 23:12:05 +0300

    w22.03

--- src/assets/js/ui/filter.js
+++ src/assets/js/ui/filter.js
@@ -15,7 +15,61 @@ export function Filter(){
 		}
 
 		const res = await service.search_ex(e.target.value)
-		console.log(Object.entries(res))
+		
+		let s = [];
+		res.forEach(el => el.schedule.map(e => {
+			e.exid = el.exid
+			e.name = el.name
+			e.distance = el.distance
+			e.duration = el.duration
+			e.uri = el.uri
+			return s.push(e)
+		}))
+		
+		let r = s.reduce((acc, current) => {
+
+			if(!acc[current.date]){
+				acc[current.date] = []
+			}
+			acc[current.date].push(current)
+			return acc
+		},[])
+
+		let ordered = []
+		let z = Object.keys(r)
+			.sort((a,b) => Date.parse(a) - Date.parse(b))
+			.forEach(k=>ordered[k] = r[k])
+		let str = ``
+		Object.keys(ordered).forEach(k => {
+			let date = k.split('.')
+			console.log(date)
+			date = `${date[0]}.${date[1]}`
+			str += `<div class="row"> <span class="date">${date}</span>`
+			
+			ordered[k].forEach(el => {
+				let adult = el.price.split(',')[0]
+				str +=`
+				<div class="record">
+					<span class="time">${el.time}</span>
+					<a href="${el.uri}">${el.name}</a>
+
+					<span class="seats">${el.seats}</span>
+						<div class="price">
+							<span byn="${adult}">${adult}</span>
+							<span class="cur">BYN</span>
+						</div>
+						<span class="book">
+							<span exid="${el.exid}" timid="${el.timid}">Бронь</span>
+						</span>
+					</div>
+				`
+			})
+			
+
+			str +=`</div>`
+		})
+
+		qs('.table .tbody').innerHTML = str
 
 	}
 
